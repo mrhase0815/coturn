2a3a369fdd629cd506cb20a45104109d768e9504
Patrick Seidler <patrick@seidler.ml>
2018-10-12 14:46:44 +0200

    copypastefehler

--- deploy_turnserver.sh
+++ deploy_turnserver.sh
@@ -1,22 +1,3 @@
-FROM ubuntu:18.04
-
-# Set the working directory to /app
-WORKDIR /app
-
-# Copy the current directory contents into the container at /app
-ADD . /app
-
-EXPOSE 3478
-
-RUN apt-get update && apt-get install -y \
-    dnsutils \
-    coturn \
-  && rm -rf /var/lib/apt/lists/*
-  
-
-ENTRYPOINT ["bash", "deploy-turnserver.sh", "$USERNAME", "$PASSWORD", "$REALM"] 
-root@nextcloud:/opt/docker-persistent-data/dockerfiles/coturn.old# cat deploy_turnserver.sh 
-
 echo $1
 echo $2
 echo $3
